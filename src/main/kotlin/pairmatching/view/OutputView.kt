0be482b957b0d4172869312f5e35aff777cd5a62
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-12 18:34:26 +0900

    feat: 매칭 결과 출력 기능

--- src/main/kotlin/pairmatching/view/OutputView.kt
+++ src/main/kotlin/pairmatching/view/OutputView.kt
@@ -2,7 +2,8 @@ package pairmatching.view
 
 class OutputView {
 
-    fun matchingResult() {
+    fun matchingResult(pairMatchingResult: List<String>) {
         println("페어 매칭 결과입니다.")
+        pairMatchingResult.forEach { result -> println(result) }
     }
 }
