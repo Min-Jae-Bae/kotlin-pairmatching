7634b71b3367ffe2e031a864e2099da97d066e6c
Min-Jae-Bae <qoalswo123@icloud.com>
2022-12-20 12:06:10 +0900

    feat: 코스, 레벨, 미션, 크루 정보 추가 및 중복 확인 기능 추가

--- src/main/kotlin/pairmatching/model/PairMatchingProcessor.kt
+++ src/main/kotlin/pairmatching/model/PairMatchingProcessor.kt
@@ -2,7 +2,9 @@ package pairmatching.model
 
 import camp.nextstep.edu.missionutils.Randoms
 import pairmatching.domain.Course
+import pairmatching.domain.Course.Companion.convertCourse
 import pairmatching.domain.Level
+import pairmatching.domain.Level.Companion.convertLevel
 import pairmatching.utils.Crew.Companion.makeCrew
 
 object PairMatchingProcessor {
@@ -10,12 +12,29 @@ object PairMatchingProcessor {
     private val matchedByLevel: HashMap<Course, HashMap<Level, HashMap<String, List<String>>>> = HashMap()
     private val matchedByMission: HashMap<Course, HashMap<String, List<List<String>>>> = HashMap()
 
+    // 매치 크루를 불러옴 (코스, 미션 값의 value 없으면 빈 리스트 반환)
+    fun getMatchedCrews(course: Course, mission: String) {
+        matchedByMission[course]?.get(mission) ?: emptyList()
+    }
+
     fun matchCrews(course: String, level: String, mission: String): List<String> {
         val shuffleCrews = getShuffledCrew(Course.convertCourse(course))
+        val matchedCrews = match(shuffleCrews)
+
+        /*
+        * TODO: 같은 레벨에서 이미 페어로 만난적이 있는 크루가 있는지 확인하는 기능
+        * TODO: 3회 시도까지 매칭 x or 매칭 경우의 수 없는지 확인하는 기능
+        *  */
+
+        addCrewByLevel(convertCourse(course), convertLevel(level), matchedCrews)
+        addCrewByMission(course, mission, matchedCrews)
+
         return emptyList()
     }
 
-    fun match(crews: List<String>): List<List<String>> {
+
+    private fun match(crews: List<String>): List<List<String>> {
+        //TODO: 최소 매칭 숫자
         val mutableCrews = crews.toMutableList()
         val matchedCrew: MutableList<List<String>> = mutableListOf()
 
@@ -29,7 +48,31 @@ object PairMatchingProcessor {
             mutableCrews.subList(0, 2).clear()
 
         }
-            return matchedCrew
+        return matchedCrew
+    }
+
+    // 레벨에 따른 (레벨 스트링과 미션을 넣는 기능 )
+    private fun addCrewByLevel(course: Course, level: Level, matchedCrew: List<List<String>>) {
+        matchedCrew.forEach { pair ->
+            pair.forEach { crew ->
+                matchedByLevel.getOrPut(course) { hashMapOf(level to hashMapOf()) }
+                matchedByLevel[course]!![level]?.getOrPut(crew) { pair }
+            }
+        }
+    }
+
+    // 코스에 따른 미션으로 크루들을 넣는 기능
+    private fun addCrewByMission(course: String, mission: String, matchedCrew: List<List<String>>) {
+        matchedByMission.getOrPut(Course.convertCourse(course)) { hashMapOf(mission to matchedCrew) }
+
+    }
+
+    // 매칭 되었는지 확인
+    fun checkMatched(course: Course, mission: String): Boolean {
+        if (matchedByMission[course]?.get(mission).isNullOrEmpty()) {
+            return false
+        }
+        return true
     }
 
 
@@ -38,5 +81,6 @@ object PairMatchingProcessor {
         matchedByMission.clear()
     }
 
-    fun getShuffledCrew(course: Course): List<String> = Randoms.shuffle(makeCrew(course))
+    private fun getShuffledCrew(course: Course): List<String> =
+        Randoms.shuffle(makeCrew(course.name))
 }
